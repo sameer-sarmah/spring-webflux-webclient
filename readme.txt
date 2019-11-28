82b4a4d5a01d240e116cb387e0be61eb2c3607e5
Sameer Sarmah <sameerparansarmah@gmail.com>
2019-11-28 20:46:05 +0530

    Adding swagger and logback.xml

---
+++ readme.txt
@@ -0,0 +1,6 @@
+Swagger API 
+http://localhost:8080/v2/api-docs
+
+
+Swagger UI
+http://localhost:8080/swagger-ui.html
--- src/main/java/northwind/config/AppConfig.java
+++ src/main/java/northwind/config/AppConfig.java
@@ -7,7 +7,13 @@ import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 
 import reactor.core.scheduler.Scheduler;
 import reactor.core.scheduler.Schedulers;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableSwagger2
 @Configuration
 @ComponentScan(basePackages= {"northwind"})
 public class AppConfig {
@@ -21,4 +27,13 @@ public class AppConfig {
     public Scheduler elasticScheduler() {
     	return Schedulers.elastic();
     }
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("northwind.controller"))
+				.paths(PathSelectors.any())
+				.build();
+	}
 }
